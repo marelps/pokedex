6fba2a984abdba4fddf484f0f4aed040e0d14051
Vitória Garrucho <vitoriagarrucho@gmail.com>
2024-10-24 16:56:54 -0300

    renderização de pokemon - imagem

--- js/script.js
+++ js/script.js
@@ -1,16 +1,45 @@
 const pokemonName = document.querySelector('.pokemon_name');
 const pokemonNumber = document.querySelector('.pokemon_number');
-const pokemonImage = document.querySelector('.pokemon_image');
+
+const form = document.querySelector('.form');
+const input = document.querySelector('.input_search');
+
+const buttonPrev = document.querySelector('.btn-prev');
+const buttonNext = document.querySelector('.btn-next');
 
 const fetchPokemon = async (pokemon) => {
-    const APIResponse = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemon}`);
-    const data = await APIResponse.json();
-    return data;
+    const APIResponse = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemon.toLowerCase()}`);
+
+    if (APIResponse == 200){
+        const data = await APIResponse.json();
+        return data;
+    }    
 }
 
 const renderPokemon = async (pokemon) => {
+    pokemonName.innerHTML = 'Loading...';
+    pokemonNumber.innerHTML = 'Wait:';
+
     const data = await fetchPokemon(pokemon);
-    pokemonName.innerHTML = data.name;
-    pokemonNumber.innerHTML = data.id;
-    pokemonImage.src = data['sprites']['versions']['generation-v']['black-white']['animated']['front_default'];
+    if (data) {
+        pokemonName.innerHTML = data.name;
+        pokemonNumber.innerHTML = data.id;
+        pokemonImage.src = data['sprites']['versions']['generation-v']['black-white']['animated']['front_default'];
+    
+        input.value = '';
+    } else {
+        pokemonName.innerHTML = 'Not found';
+        pokemonNumber.innerHTML = '):';
+    }
 }
+
+form.addEventListener('submit', (event) => {
+    event.preventDefault();
+    renderPokemon(input.value);
+});
+
+buttonPrev.addEventListener('click', () => {
+    
+});
+
+renderPokemon('1');
